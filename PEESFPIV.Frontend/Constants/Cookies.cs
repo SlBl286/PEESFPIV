720821e0a58b717c8dcb763c5c875fe9c1526e69
qy286 <quy.doanduy@gmail.com>
2025-01-15 17:29:21 +0700

    UPDATE

--- PEESFPIV.Frontend/Constants/Cookies.cs
+++ PEESFPIV.Frontend/Constants/Cookies.cs
@@ -2,6 +2,6 @@ namespace PEESFPIV.Frontend.Constants;
 
 public static class Cookies
 {
-    public static readonly string AuthScheme = "PEESFPIV-AUTH"; 
-public static readonly string AuthCoockie = "PEESFPIV-COOKIES";
+    public static readonly string AuthScheme = "PEESFPIV-AUTH";
+    public static readonly string AuthCoockie = "PEESFPIV-COOKIES";
 }
--- PEESFPIV.Frontend/Program.cs
+++ PEESFPIV.Frontend/Program.cs
@@ -3,6 +3,7 @@ using PEESFPIV.Frontend.Constants;
 using PEESFPIV.Frontend.Databases;
 using PEESFPIV.Frontend.States;
 using PEESFPIV.Frontend.Utils;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,24 +13,40 @@ builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 builder.Services.AddStates();
 builder.Services.AddUtils();
 builder.Services.AddBlazorBootstrap();
-builder.Services.AddCascadingAuthenticationState();
+
 builder.Services.AddDatabase(builder.Configuration);
+builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
 
 builder.Services.AddAuthentication(Cookies.AuthScheme)
                 .AddCookie(Cookies.AuthScheme, options =>
                 {
                     options.Cookie.Name = Cookies.AuthCoockie;
+                    options.Cookie.Path = "/";
                     options.LoginPath = "/cms/auth/Login";
                     options.AccessDeniedPath = "/cms/auth/Access-Denied";
                     options.LogoutPath = "/cms/auth/Logout";
 
                     options.Cookie.HttpOnly = true;
                     options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
-                    options.Cookie.SameSite = SameSiteMode.Strict;
+                    options.Cookie.SameSite = SameSiteMode.Unspecified;
 
                     options.ExpireTimeSpan = TimeSpan.FromDays(2);
                     options.SlidingExpiration = true;
                 });
+
+builder.Services.AddAuthorization();
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddCors(options =>
+{
+     options.AddPolicy("AuthOrigins",
+                           policy =>
+                           {
+                                policy.WithOrigins("http://192.168.0.23:5249")
+                                                   .AllowAnyHeader()
+                                                   .AllowAnyMethod()
+                                                   .AllowCredentials();
+                           });
+});
 
 
 var app = builder.Build();
@@ -41,15 +58,16 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.UseStaticFiles();
+app.UseCors("AuthOrigins");
 
 app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-
 app.UseAuthentication()
     .UseAuthorization();
 
+
 app.UseAntiforgery();
+
 
 app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
 
